45fa973451ee5a6aab138523f6621feda3c724dc
alemalagni <alemalagni@gmail.com>
2025-09-19 16:59:16 +0200

    refactor: memoize NavBar component for performance optimization

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
+import { memo } from "react";
 import "../css/NavBar.css";
 
-export default function NavBar() {
+export default memo(function NavBar() {
     return (
         <nav className="navbar">
             <a href="/">Task List</a>
             <a href="/addTask">Add Task</a>
         </nav>
     )
-}
+})
