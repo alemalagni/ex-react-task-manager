668f6cf20af6afda0c4e840ddc864b43b736ac56
alemalagni <alemalagni@gmail.com>
2025-09-19 12:09:07 +0200

    feat: add styling to NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
+import "../css/NavBar.css";
+
 export default function NavBar() {
     return (
-        <nav>
+        <nav className="navbar">
             <a href="/">Task List</a>
             <a href="/addTask">Add Task</a>
         </nav>
