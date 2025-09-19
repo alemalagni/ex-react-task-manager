a4d3072ccb16b3632e22ac5e988093dd7b6cd072
alemalagni <alemalagni@gmail.com>
2025-09-19 11:56:36 +0200

    feat: add NavBar component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddTask from "./pages/AddTask";
 import TaskList from "./pages/TaskList";
+import NavBar from "./components/NavBar";
 import './App.css'
 
 function App() {
   return (
     <>
+      <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<TaskList />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <a href="/">Task List</a>
+            <a href="/addTask">Add Task</a>
+        </nav>
+    )
+}
