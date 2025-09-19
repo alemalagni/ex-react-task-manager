ab3c2efc391c3c3f9c78dd183b961db5ed5426cc
alemalagni <alemalagni@gmail.com>
2025-09-19 11:51:54 +0200

    feat: update routing and fix AddTask component title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddTask from "./pages/AddTask";
+import TaskList from "./pages/TaskList";
 import './App.css'
 
 function App() {
-
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/addTask" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,7 +1,7 @@
 export default function AddList() {
     return (
         <div>
-            <h1>Add List</h1>
+            <h1>Add Task</h1>
         </div>
     )
 }
