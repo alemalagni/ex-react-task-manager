ab8793298a732b2e218f0d4e0d152a1c5e5e9cae
alemalagni <alemalagni@gmail.com>
2025-09-19 23:36:39 +0200

    refactor: enhance TaskRow component with link styling and remove console log

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,13 +1,13 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
+import "../css/TaskRow.css";
 
 function TaskRow({ task }) {
-    console.log("Task:", task.id);
     return (
         (<tr key={task.id}>
-            <td><Link to={`/task/${task.id}`}>{task.title}</Link></td>
-            <td className={` ${task.status} `}>{task.status}</td>
-            <td>{new Date(task.createdAt).toLocaleDateString() + " " + new Date(task.createdAt).toLocaleTimeString()}</td>
+            <td><Link to={`/task/${task.id}`}><span className="link-text">{task.title}</span></Link></td>
+            <td className={`${task.status} `}>{task.status}</td>
+            <td>{new Date(task.createdAt).toLocaleDateString()}</td>
         </tr>)
     );
 }
