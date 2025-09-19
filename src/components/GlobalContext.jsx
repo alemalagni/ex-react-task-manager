d3a75aba80d5dbfeff9c8941b32f243d2d7dfb6e
alemalagni <alemalagni@gmail.com>
2025-09-19 12:26:44 +0200

    fix: update GlobalProvider to accept children and improve task fetching logic

--- src/components/GlobalContext.jsx
+++ src/components/GlobalContext.jsx
@@ -2,19 +2,22 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const GlobalContext = createContext();
 
-export function GlobalProvider() {
+export function GlobalProvider({ children }) {
     const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
         fetch(`${process.env.URL}/tasks`)
             .then(res => res.json())
-            .then(data => setTasks(data))
+            .then(data => {
+                setTasks(data),
+                    console.log(data);
+            })
             .catch(err => console.error("Error fetching tasks:", err));
     }, []);
 
     return (
         <GlobalContext.Provider value={{ tasks, setTasks }}>
-            {console.log(tasks)}
+            {children}
         </GlobalContext.Provider>
     );
 }
